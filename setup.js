9c96dc2a3eace63dd43b4a4a45505336ca62b0a8
heddaenger <hedda.2111@live.no>
2020-04-23 11:53:38 +0200

    I have updatet setup.js so the code is now working. Made a html and js file for registration, just a beginning, they are currently not working and not finish.

--- setup.js
+++ setup.js
@@ -1,10 +1,11 @@
+const pool = require('./routes/index');
 pool.query(`
 CREATE TABLE Users (
 id SERIAL PRIMARY KEY, 
-fullName text VARCHAR (100) NOT NULL, 
-email text VARCHAR (100) NOT NULL, 
-password text VARCHAR (50) NOT NULL, 
-phone INT NOT NULL); 
+fullName text NOT NULL, 
+email text NOT NULL, 
+password text NOT NULL, 
+phone INT NOT NULL);
 
 CREATE TABLE Booking (
 id SERIAL PRIMARY KEY,
@@ -13,14 +14,14 @@ date DATE NOT NULL,
 time INT NOT NULL,
 Users_id INTEGER REFERENCES Users(id) ON DELETE CASCADE);
 
-CREATE TABLE AdminUser (
+CREATE TABLE adminUser (
 id SERIAL PRIMARY KEY, 
-adminUsername text VARCHAR (50) NOT NULL, 
-adminPassword text VARCHAR (50) NOT NULL); 
-
-INSERT INTO TABLE Users(...) VALUES (...);
+fullName text NOT NULL, 
+email text NOT NULL, 
+password text NOT NULL, 
+phone INT NOT NULL);
 
 `).then(result => {
-    console.log(error, result);
+    console.log(result);
 });
 
